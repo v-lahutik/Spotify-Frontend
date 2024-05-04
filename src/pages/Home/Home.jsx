1a9c8e45617dda76da18a51d10684e39d88a7426
Snezana1412 <snezana.lalatovic@dci-student.org>
2024-05-04 08:36:57 +0200

    Created basic Home layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,23 @@
 import { Header } from '../../components';
+import Footer from '../../components/Footer/Footer';
+import Main from '../../components/Main/Main';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 export const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <div className="spotify-body">
+        <Sidebar />
+        <div className="body">
+          <Header />
+          <div className="body-contents">
+            <Main />
+          </div>
+        </div>
+      </div>
+      <div className="spotify-footer">
+        <Footer />
+      </div>
+    </>
+  );
 };
