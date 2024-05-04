1cdd47ea7f40bf060d754b0cd0071d925581e497
Snezana1412 <snezana.lalatovic@dci-student.org>
2024-05-04 07:03:41 +0200

    Added Spotify service and Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet } from 'react-router-dom';
 import { Home } from './pages';
+import Login from './pages/Login/Login';
 
 const Root = () => {
   return (
@@ -13,7 +14,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Root />}>
-        <Route index element={<Home />} />
+        {/* <Route index element={<Home />} /> */}
+        <Route index element={<Login />} />
       </Route>
     </>,
   ),
