246d01dc2ec93b318b6fbda12cce3c3820421130
Snezana1412 <snezana.lalatovic@dci-student.org>
2024-05-15 14:25:20 +0200

    Style sidebar and fetch categories playlists

---
+++ src/services/categories.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import fetchData from '../utils/fetchData';
+
+export default function useCategoriesPlaylists() {
+  const [categoriesPlaylists, setCategoriesPlaylists] = useState(null);
+
+  useEffect(() => {
+    fetchData(`https://api.spotify.com/v1/browse/categories`).then((data) => {
+      setCategoriesPlaylists(data);
+      console.log('🚀 ~ fetchData ~ data:', data);
+    });
+  }, []);
+
+  return categoriesPlaylists;
+}
